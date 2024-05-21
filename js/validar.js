856155a5ca890a2ba334acb0a982791fe98fe5a0
PTurquetti <pauloturquetti29@gmail.com>
2024-05-21 11:50:15 -0300

    Novo commit

--- js/validar.js
+++ js/validar.js
@@ -37,7 +37,7 @@ function validarForm(e){
     const senhaValido = validarSenha({ target: senha });
 
     if(nomeValido && anoValido && emailValido && senhaValido){
-        resultado.innerText = "Seus dados foram registrados";
+        resultado.innerText = "Parabéns seus dados foram registrados :)";
         resultado.style.color = "green";
     }else{
         resultado.innerText = "Seus dados não foram registrados";
@@ -54,7 +54,7 @@ function validarNome(e){
     
     if(e.target.value.trim().match(regexNome)==null){
         //Se não cumpre a expressao regular
-        nomeHelp.textContent = "Nome inválido"; 
+        nomeHelp.textContent = "Nome inválido!"; 
         nomeHelp.style.color="red";
         return false;
     }
@@ -74,7 +74,7 @@ function validarAno(e){
 
     if(anoTrimado.match(regexAno)==null){
         // Se o ano digitado não está no intervale
-        anoHelp.textContent = "Ano inválido";
+        anoHelp.textContent = "Ano inválido!";
         anoHelp.style.color="red";
         return false;
     }
@@ -93,7 +93,7 @@ function validarEmail(e){
 
     if(e.target.value.trim().match(regexEmail)==null){
         // Se não cumpre a expressão regular - Ano inválido
-        emailHelp.textContent = "Formato de email inválido";
+        emailHelp.textContent = "Formato de email inválido!";
         emailHelp.style.color="red";
         return false;
     }
@@ -119,7 +119,7 @@ function validarSenha(e){
     // Testando tamanho
     if(senha.length < 6 || senha.length > 20){
         //Não atinge tamanho mínimo ou excede o tamanho máximo
-        senhaHelp.textContent = "Senha inválida";
+        senhaHelp.textContent = "Senha inválida!";
         senhaHelp.style.color="red";
         return false;
     }else{
@@ -130,13 +130,13 @@ function validarSenha(e){
         e.target.value.trim().match(regexNumero)==null ||
         e.target.value.trim().match(regexLetra)==null){
             //Se não tem ocorrencia de algum desses
-            senhaHelp.textContent = "Senha inválida";
+            senhaHelp.textContent = "Senha inválida!";
             senhaHelp.style.color="red";
             return false;
         }
         else if(senha.indexOf(nome.value.trim()) !== -1 || senha.indexOf(ano.value.trim()) !== -1){
             //Se a senha contem a substring inserida no Nome ou no Ano
-            senhaHelp.textContent = "Senha inválida";
+            senhaHelp.textContent = "Senha inválida!";
             senhaHelp.style.color="red";
             return false;
             
